120fffa728451c3d69e9d5646cf556045deb24a7
John Sloan <jsloan256@gmail.com>
2014-06-09 19:20:34 -0500

    Started added a counter controlled shift enable signal to slow the shift down so that it is visible on a set of LEDs.

--- wiggle/wiggle.v
+++ wiggle/wiggle.v
@@ -1,19 +1,46 @@
-module wiggle (gpio, clk, rstn);
+module wiggle (clk, rstn, gpio, count);
 
-output [7:0] gpio;
 input clk, rstn;
-reg [7:0] register;
+output [7:0] gpio;
+output [26:0] count;
+
+reg [26:0] count;
+reg [7:0] sreg;
+reg shift;
 wire [7:0] gpio;
 
 always @(posedge clk or posedge rstn)
 begin
 	if (rstn == 0) begin
-		register <= 1;
+		count <= 0;
 	end else begin
-	register <= register << 1;
+		count <= count + 1;
 	end
 end
 
-assign gpio = register;
+always @(posedge clk or posedge rstn)
+begin
+	if (rstn == 0) begin
+		shift = 0;
+	end if (count == 1) begin
+		shift = 1;
+	end else begin
+		shift = 0;
+	end
+end
+
+always @(posedge clk or posedge rstn)
+begin
+	if (rstn == 0) begin
+		sreg <= 1;
+	end if (shift == 1) begin
+		sreg <= sreg << 1;
+		sreg[0] <= sreg[7];
+	end else begin
+		sreg <= sreg;
+	end
+end
+
+assign gpio = sreg;
 
 endmodule
--- wiggle/wiggle_tb.v
+++ wiggle/wiggle_tb.v
@@ -1,22 +1,23 @@
+`timescale 1 ns / 100 ps
+
 module test;
 
 wire [7:0] gpio;
+wire [26:0] count;
 
   /* Make a rstn that pulses once. */
   reg rstn = 0;
   initial begin
-	$monitor("clk=%b, rstn=%b, gpio=%b", clk, rstn, gpio);
-     # 17 rstn = 1;
-     # 11 rstn = 0;
-     # 29 rstn = 1;
-     # 11 rstn = 0;
-     # 100 $stop;
+	$monitor("time=%10d, clk=%b, rstn=%b, gpio=%b, count=%b", $time, clk, rstn, gpio, count);
+     # 0 rstn = 0;
+     # 13 rstn = 1;
+     # 100 $finish;
   end
 
   /* Make a regular pulsing clock. */
   reg clk = 0;
   always #5 clk = !clk;
 
-  wiggle u1 (gpio, clk, rstn);
+  wiggle u1 (clk, rstn, gpio, count);
 
 endmodule // test
